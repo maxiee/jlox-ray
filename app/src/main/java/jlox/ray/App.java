b8497257c26bd8dada0b454d8a1b81b490b13284
maxiee <maxieewong@gmail.com>
2022-10-13 09:29:59 +0800

    report and error

--- app/src/main/java/jlox/ray/App.java
+++ app/src/main/java/jlox/ray/App.java
@@ -48,4 +48,14 @@ public class App {
              runPrompt();
         }
     }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        // hadError = true;
+    }
 }

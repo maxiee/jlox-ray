52baf60de465d9c97a3a43f44bb8baf698777648
maxiee <maxieewong@gmail.com>
2022-10-14 15:48:12 +0800

    generate ast

--- app/src/main/java/jlox/ray/GenerateAst.java
+++ app/src/main/java/jlox/ray/GenerateAst.java
@@ -26,11 +26,16 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> ivsitor);");
 
         writer.println("}");
         writer.close();
@@ -49,11 +54,27 @@ public class GenerateAst {
         }
         writer.println("        }");
 
+        // Visitor pattern
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
         // fields
         for (String field: fields) {
             writer.println("        final " + field + ";");
         }
 
+        writer.println("    }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
         writer.println("    }");
     }
 }

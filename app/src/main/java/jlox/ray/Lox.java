d9b0ad36533d33c496299bbe0907f6c4f03d347a
maxiee <maxieewong@gmail.com>
2022-10-23 00:36:56 +0800

    interpreter.interpret

--- app/src/main/java/jlox/ray/Lox.java
+++ app/src/main/java/jlox/ray/Lox.java
@@ -12,7 +12,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
@@ -24,13 +27,15 @@ public class Lox {
         // stop if there was a syntax error
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
+        //System.out.println(new AstPrinter().print(expression));
     }
 
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -73,6 +78,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {

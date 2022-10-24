324e733000ef5a3acc421b0dd051c610f18b4a89
maxiee <maxieewong@gmail.com>
2022-10-24 13:07:20 +0800

    Environment enclosing

--- app/src/main/java/jlox/ray/Environment.java
+++ app/src/main/java/jlox/ray/Environment.java
@@ -4,7 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+
+    final Environment enclosing;
+
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value) {
         values.put(name, value);
@@ -13,6 +24,10 @@ public class Environment {
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
+        }
+
+        if (enclosing != null) {
+            return enclosing.get(name);
         }
 
         throw new RuntimeError(
@@ -24,6 +39,10 @@ public class Environment {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
             return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
         }
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");

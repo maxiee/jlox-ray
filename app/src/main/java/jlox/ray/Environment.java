8c43a074e112146effe81dc83cdab3719b329aea
maxiee <maxieewong@gmail.com>
2022-10-25 09:40:19 +0800

    bug fix

--- app/src/main/java/jlox/ray/Environment.java
+++ app/src/main/java/jlox/ray/Environment.java
@@ -43,6 +43,7 @@ public class Environment {
 
         if (enclosing != null) {
             enclosing.assign(name, value);
+            return;
         }
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
